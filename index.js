dfa3ecf5c33f0d7ce024371b60a6eeb79e044aa4
Anirudh Kulkarni <k.anirudh8@gmail.com>
2017-02-21 00:54:16 +0530

    Added offer #3

--- index.js
+++ index.js
@@ -88,36 +88,27 @@ function sendTextMessage(sender, text) {
 
 function sendGenericMessage(sender) {
 	let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "Oculus Rift",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+					"buttons": [{
+						"type": "postback",
+						"title": "Accept Offer",
+            "payload": "OFFER_ACCEPTED"
+					}, {
+						"type": "postback",
+						"title": "Reject Offer",
+						"payload": "OFFER_REJECTED",
+					}],
+				}]
+			}
+		}
+	}
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {access_token:token},
