6ed6188fc59bf85b1e900cc17d015d5fd359bc23
Anirudh Kulkarni <k.anirudh8@gmail.com>
2017-02-23 23:22:59 +0530

    Minor changes

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post('/webhook/', function (req, res) {
 			} 
 
 			else if(NBA === "abcdefxyz" || text.includes('offer') || text.includes('plan') || text.includes('deal') || text.includes('Deal')|| text.includes('Offer')|| text.includes('Plan')) {
-				sendTextMessage(sender, "Sure +"+ user_name +"+, Let me check what kind of offers I have got in store for you.", token)
+				sendTextMessage(sender, "Sure "+ user_name +", Let me check what kind of offers I have got in store for you.", token)
 				sendBestOffer(sender)
 				sendTextMessage(sender, "As you have crossed the data usage threshold in the past two month, I suggested to you this offer.", token)
 				// sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
