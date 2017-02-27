83772b85533ce5ad5c0fd0d1ac6a926beffbd594
Anirudhk94 <k.anirudh8@gmail.com>
2017-02-27 14:28:35 +0530

    Minor changes

--- index.js
+++ index.js
@@ -416,9 +416,9 @@ function sendRecommendedBundle(sender, bundle) {
 			sendTextMessage(sender,"Total pay(now) : $121" )
 			sendTextMessage(sender," Total pay(monthly) : $73 ")
 			setTimeout(function(){
-				sendTextMessage(sender, "Current subscription, "+<center></center>+" Total pay(monthly) : $94")
+				sendTextMessage(sender, "Current subscription charges per month : $94")
 				bundleDecision(sender, bundle);
-			}, 3000)
+			}, 4000)
 			
 		}
 	})
